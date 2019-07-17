c791f7bcbcd187263c04a6f0323957da85208359
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-07-17 15:53:29 -0700

    Photo, title, and copyright all pulling through; need to get useEffect going

--- src/components/body.js
+++ src/components/body.js
@@ -6,20 +6,26 @@ import ImgCard from "./imgCard.js";
 export default function ImgContainer() {
 
   const [imgURL, setImgURL] = useState([])
+  const [imgTitle, setImgTitle] = useState([])
+  const [imgCopyright, setimgCopyright] = useState([])
 
     axios
     .get(`https://api.nasa.gov/planetary/apod?api_key=pe1nx4Aolg4EtkBvJ8ZwQRAqnI7M8eE7obEJubog&date=2012-03-14`)
     .then(response => {
       const urlFromNasa = response.data.url
-      console.log("data from NASA (response.data.url):", response.data.url)
+      const titleFromNasa = response.data.title
+      const imgCopyright = response.data.copyright
+      console.log("data from NASA (response.data):", response.data)
       setImgURL(urlFromNasa)
+      setImgTitle(titleFromNasa)
+      setimgCopyright(imgCopyright)
     })
 
     return (
       <div className="img">
-        <ImgCard imgURL={imgURL} />
-        {console.log("in body.js return", {imgURL})}
+        <ImgCard imgURL={imgURL} imgTitle={imgTitle} imgCopyright={imgCopyright}/>
       </div>
+
     )  
 }
 
--- src/components/imgCard.js
+++ src/components/imgCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function ImgCard({ imgURL }) {
+export default function ImgCard({ imgURL, imgTitle, imgCopyright }) {
     return (
       <div className="img-card">
         <img src={imgURL} alt="text" style={{ maxWidth: "275px" }} />
+        <h3>Image Title: {imgTitle} </h3>
+        <h4>Copyright: {imgCopyright} </h4>
       </div>
     );
 }
