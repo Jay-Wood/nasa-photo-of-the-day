05655f5c496c2df5f41374f2a9f738e6e6c690f0
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-07-18 12:45:07 -0700

    About to start day 2 styling.

--- src/components/body.js
+++ src/components/body.js
@@ -8,11 +8,14 @@ export default function ImgContainer() {
   const [imgURL, setImgURL] = useState([])
   const [imgTitle, setImgTitle] = useState([])
   const [imgCopyright, setimgCopyright] = useState([])
+  // const [photoObject, setPhotoObj] = useState({})
 
   useEffect( () => {
     axios
       .get(`https://api.nasa.gov/planetary/apod?api_key=pe1nx4Aolg4EtkBvJ8ZwQRAqnI7M8eE7obEJubog&date=2012-03-14`)
       .then(response => {
+        //const photoObject = response.data
+        //setphotoObj(response.data)
         const urlFromNasa = response.data.url
         const titleFromNasa = response.data.title
         const imgCopyright = response.data.copyright
@@ -24,6 +27,7 @@ export default function ImgContainer() {
     }, [])
 
     return (
+      //props from photoObject here? 
       <div className="img">
         <ImgCard imgURL={imgURL} imgTitle={imgTitle} imgCopyright={imgCopyright}/>
       </div>
--- src/components/imgCard.js
+++ src/components/imgCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+//pull out photoObject props here? 
+
 export default function ImgCard(props) {
     return (
       <div className="img-card">
-        <img src={props.imgURL} alt="text" style={{ maxWidth: "275px" }} />
+        <img src={props.imgURL} alt="text" style={{ maxWidth: "575px" }} />
         <h3>Image Title: {props.imgTitle} </h3>
         <h4>Copyright: {props.imgCopyright} </h4>
       </div>
